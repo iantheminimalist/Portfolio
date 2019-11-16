2e37957326750746cccec2a10752fdeb18111c7a
rorypond <ian.dejesus.fontilea@gmail.com>
2019-11-16 01:02:20 -0800

    initaialized the font to the styles files.

---
+++ js/navbar.js
@@ -0,0 +1,17 @@
+const navDrop = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav_links');
+   
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+   });
+
+
+
+}
+
+const App = () => {
+    navDrop();
+}
+
+App();
